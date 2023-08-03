aadd649ebba2382d0af5717265b4d20a13077ad9
Shabrina99 <118566547+Shabrina99@users.noreply.github.com>
2023-08-03 10:58:03 -0400

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_Post['email'];
 $subject_email = $_Post['subject'];
 $message_email = $_Post['message'];
 
-$email_form = 'shabrinayesmin99@gmail.com';
+$email_form = 'info@shabrinayesmin.com';
 
 $email_subject = 'New Form Submission';
 
