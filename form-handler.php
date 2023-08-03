febca18f759f7ef91f95a00b27a296cd96d6d989
Shabrina99 <118566547+Shabrina99@users.noreply.github.com>
2023-08-02 23:58:17 -0400

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_Post['name'];
+$visitor_email = $_Post['email'];
+$subject_email = $_Post['subject'];
+$message_email = $_Post['message'];
+
+$email_form = 'shabrinayesmin99@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $message.\n";
+
+$to = 'shabrinayesmin99@gmail.com';
+$headers = "Form: $email_form \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+              
+?>
